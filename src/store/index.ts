bb10e319b6f2d6e6038e7e22c63a14882a8cb7fe
Tonia-SE <64790742+Tonia-SE@users.noreply.github.com>
2021-03-19 18:22:15 +0300

    fix: add lint script

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,9 +3,9 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/rootReducer';
 
 declare global {
-	export interface Window {
-		__REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-	}
+  export interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
 }
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
--- src/store/types.ts
+++ src/store/types.ts
@@ -1,33 +1,33 @@
 export interface IWord {
-	id: string;
-	group: 0;
-	page: 0;
-	word: string;
-	image: string;
-	audio: string;
-	audioMeaning: string;
-	audioExample: string;
-	textMeaning: string;
-	textExample: string;
-	transcription: string;
-	wordTranslate: string;
-	textMeaningTranslate: string;
-	textExampleTranslate: string;
+  id: string;
+  group: 0;
+  page: 0;
+  word: string;
+  image: string;
+  audio: string;
+  audioMeaning: string;
+  audioExample: string;
+  textMeaning: string;
+  textExample: string;
+  transcription: string;
+  wordTranslate: string;
+  textMeaningTranslate: string;
+  textExampleTranslate: string;
 }
 
 export interface IDictionary {
-	words: IWord[];
-	wordsActual: IWord[];
-	wordsDifficult: IWord[];
-	wordsLearned: IWord[];
+  words: IWord[];
+  wordsActual: IWord[];
+  wordsDifficult: IWord[];
+  wordsLearned: IWord[];
 }
 
 export interface IAppState {
-	dictionary: IDictionary;
-	isLoggedIn?: boolean;
-	isRegistred?: boolean;
-	userName?: string;
-	userImage?: string;
+  dictionary: IDictionary;
+  isLoggedIn?: boolean;
+  isRegistred?: boolean;
+  userName?: string;
+  userImage?: string;
 }
 
 export enum DictionaryActionTypes {
