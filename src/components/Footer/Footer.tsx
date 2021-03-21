9714794aaef67777c554a34cbe77e223b77b2e49
Alexey Teterin <teterin.alexey@gmail.com>
2021-03-21 16:52:54 +0500

    refactor: move URL and AUTHORS to constants.ts

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,29 +1,17 @@
 import React from 'react';
 import { Link, Typography } from '@material-ui/core';
+import { URLs, AUTHORS } from './constants';
 import { Author } from './types';
 import './Footer.scss';
 
 const Footer: React.FC = () => {
-  const URL = {
-    github: 'https://github.com/',
-    rsschool: 'https://rs.school/',
-  };
-  const authors: Author[] = [
-    { name: 'Larisa Arkaeva', githubName: 'lbratkovskaya' },
-    { name: 'Maxim Chernov', githubName: 'blackymax' },
-    { name: 'Nick Gurinovich', githubName: 'Gurnick013' },
-    { name: 'Aliaksei Latypau', githubName: 'alexlatypov94' },
-    { name: 'Alexey Teterin', githubName: 'AlexeyTeterin' },
-    { name: 'Antonina Tregubova', githubName: 'Tonia-SE' },
-  ];
-
   const renderGithubLink = (author: Author) => {
     const [firstName, surname] = author.name.split(' ');
 
     return (
       <Typography key={author.githubName}>
         <Link
-          href={`${URL.github}${author.githubName}`}
+          href={`${URLs.github}${author.githubName}`}
           target="_blank"
           rel="noreferrer"
           className="github-link">
@@ -34,12 +22,12 @@ const Footer: React.FC = () => {
   };
 
   const renderAuthors = (
-    <div className="authors">{authors.map((author) => renderGithubLink(author))}</div>
+    <div className="authors">{AUTHORS.map((author) => renderGithubLink(author))}</div>
   );
 
   return (
     <footer className="footer">
-      <a className="rsschool" href={URL.rsschool} target="_blank" rel="noreferrer">
+      <a className="rsschool" href={URLs.rsschool} target="_blank" rel="noreferrer">
         {' '}
       </a>
       {renderAuthors}
