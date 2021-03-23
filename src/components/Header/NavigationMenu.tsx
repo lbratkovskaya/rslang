b3645b0fb8c6129eb2a321465a6edba35b08f352
lbratkovskaya <lbratkovskaya@naumen.ru>
2021-03-23 23:10:24 +0500

    fix: prettiers

--- src/components/Header/NavigationMenu.tsx
+++ src/components/Header/NavigationMenu.tsx
@@ -1,59 +1,29 @@
-import React, {
-  RefObject,
-  useRef,
-  useState,
-} from 'react';
+import React, { RefObject, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
-import {
-  AppBar,
-  Menu,
-  MenuItem,
-  Toolbar,
-  IconButton,
-} from '@material-ui/core';
+import { AppBar, Menu, MenuItem, Toolbar, IconButton } from '@material-ui/core';
 import { ArrowDropDown, MoreVert } from '@material-ui/icons';
 import { menuItems, mobileMenuId } from './navMenuData';
-import {
-  IMenuItem,
-  MobileNavMenuProps,
-  NavSubMenuProps,
-} from './types';
+import { IMenuItem, MobileNavMenuProps, NavSubMenuProps } from './types';
 import useStyles from './classes';
 
 /* ===========common methods============ */
 const generateRefToRefObject = (
   refsObject: RefObject<{ [key: string]: HTMLDivElement }>,
   element: HTMLDivElement,
-  menuItem: IMenuItem,
-) => Object.assign(
-  refsObject.current,
-  { [menuItem.id]: element },
-);
+  menuItem: IMenuItem
+) => Object.assign(refsObject.current, { [menuItem.id]: element });
 
 const NavSubMenu: React.FC<NavSubMenuProps> = (props: NavSubMenuProps) => {
-  const {
-    anchor,
-    id,
-    isOpen,
-    items,
-    onMenuClose,
-  } = props;
+  const { anchor, id, isOpen, items, onMenuClose } = props;
 
   const subMenuClasses = useStyles();
 
-  const renderItems = () => items?.map((item) => (
-    <MenuItem
-      className={subMenuClasses.navSubMenuItem}
-      key={item.label}
-      onClick={onMenuClose}
-    >
-      {
-        item.withLink
-          ? <Link to={item.linkAddress}>{item.label}</Link>
-          : item.label
-      }
-    </MenuItem>
-  ));
+  const renderItems = () =>
+    items?.map((item) => (
+      <MenuItem className={subMenuClasses.navSubMenuItem} key={item.label} onClick={onMenuClose}>
+        {item.withLink ? <Link to={item.linkAddress}>{item.label}</Link> : item.label}
+      </MenuItem>
+    ));
 
   return (
     <Menu
@@ -64,55 +34,96 @@ const NavSubMenu: React.FC<NavSubMenuProps> = (props: NavSubMenuProps) => {
       keepMounted
       transformOrigin={{ vertical: 'top', horizontal: 'right' }}
       open={isOpen}
-      onClose={onMenuClose}
-    >
+      onClose={onMenuClose}>
       {items && renderItems()}
     </Menu>
   );
 };
 
 const renderMenuItem = (
-  refsObject: RefObject<{ [key: string]: HTMLDivElement }> = { current: {} = {} },
+  refsObject: RefObject<{ [key: string]: HTMLDivElement }>,
   menuItem: IMenuItem,
   itemClassName: string,
   subMenuOpenId: string,
   setSubMenuOpenId: (id: string) => void,
-  handleMenuClose: () => void,
+  handleMenuClose: () => void
 ) => {
   const buttonClickEventHandler = (event: React.MouseEvent) => {
     event.stopPropagation();
     setSubMenuOpenId(menuItem.subMenuId || '');
   };
 
-  return (<div
-    key={menuItem.id}
-    className={itemClassName}
-    ref={(element: HTMLDivElement) =>
-      generateRefToRefObject(refsObject, element, menuItem)}
-  >
-    <Link to={{ pathname: menuItem.linkAddress }}>{menuItem.label}</Link>
-    {menuItem.withSubMenu && (
-      <>
-        <IconButton
-          aria-label={menuItem.label}
-          aria-controls={menuItem.subMenuId}
-          aria-haspopup="true"
-          onClick={buttonClickEventHandler}
-        >
-          <ArrowDropDown />
-        </IconButton>
-        <NavSubMenu
-          anchor={refsObject.current![menuItem.id]}
-          id={menuItem.subMenuId!}
-          isOpen={subMenuOpenId === menuItem.subMenuId}
-          items={menuItem.subMenuItems!}
-          onMenuClose={handleMenuClose}
-        />
-      </>
-    )
-    }
-  </div >);
-}
+  return (
+    <div
+      key={menuItem.id}
+      className={itemClassName}
+      ref={(element: HTMLDivElement) => generateRefToRefObject(refsObject, element, menuItem)}>
+      <Link to={{ pathname: menuItem.linkAddress }}>{menuItem.label}</Link>
+      {menuItem.withSubMenu && (
+        <>
+          <IconButton
+            aria-label={menuItem.label}
+            aria-controls={menuItem.subMenuId}
+            aria-haspopup="true"
+            onClick={buttonClickEventHandler}>
+            <ArrowDropDown />
+          </IconButton>
+          <NavSubMenu
+            anchor={refsObject.current![menuItem.id]}
+            id={menuItem.subMenuId!}
+            isOpen={subMenuOpenId === menuItem.subMenuId}
+            items={menuItem.subMenuItems!}
+            onMenuClose={handleMenuClose}
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+export const MobileNavMenu: React.FC<MobileNavMenuProps> = (props: MobileNavMenuProps) => {
+  const [subMenuOpenId, setSubMenuOpenId] = useState<string>('');
+
+  const refsObject = useRef<{ [key: string]: HTMLDivElement }>({});
+
+  const handleSubMenuClose = () => {
+    setTimeout(() => setSubMenuOpenId('none'), 0);
+  };
+
+  const { anchor, isOpen, items, onMenuClose } = props;
+
+  const mobileClasses = useStyles();
+
+  const renderItems = () =>
+    items?.map((menuItem: IMenuItem) => {
+      return (
+        <MenuItem key={menuItem.id} onClick={onMenuClose}>
+          {renderMenuItem(
+            refsObject,
+            menuItem,
+            mobileClasses.navMenuItem,
+            subMenuOpenId,
+            setSubMenuOpenId,
+            handleSubMenuClose
+          )}
+        </MenuItem>
+      );
+    });
+
+  return (
+    <Menu
+      anchorEl={anchor}
+      getContentAnchorEl={null}
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      id="menu-auth"
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isOpen}
+      onClose={onMenuClose}>
+      {items && renderItems()}
+    </Menu>
+  );
+};
 
 export const NavigationMenu: React.FC = () => {
   const [subMenuOpenId, setSubMenuOpenId] = useState<string>('');
@@ -135,115 +146,58 @@ export const NavigationMenu: React.FC = () => {
     setMobileMenuOpen(true);
   };
 
-  const renderMenuItems = () => menuItems.map((menuItem) => {
-    const getRef = (element: HTMLDivElement) =>
-      Object.assign(refsObject.current, { [menuItem.id]: element });
-    return (
-      <div key={menuItem.id} className={classes.navMenuItem} ref={getRef}>
-        <Link to={menuItem.linkAddress}>{menuItem.label}</Link>
-        {menuItem.withSubMenu && (
-          <>
-            <IconButton
-              aria-label={menuItem.label}
-              aria-controls={menuItem.subMenuId}
-              aria-haspopup="true"
-              onClick={() => setSubMenuOpenId(menuItem.subMenuId || '')}
-            >
-              <ArrowDropDown />
-            </IconButton>
-            <NavSubMenu
-              anchor={refsObject.current[menuItem.id]}
-              id={menuItem.subMenuId!}
-              isOpen={subMenuOpenId === menuItem.subMenuId}
-              items={menuItem.subMenuItems!}
-              onMenuClose={handleMenuClose}
-            />
-          </>
-        )}
-      </div>
-    );
-  });
+  const renderMenuItems = () =>
+    menuItems.map((menuItem) => {
+      const getRef = (element: HTMLDivElement) =>
+        Object.assign(refsObject.current, { [menuItem.id]: element });
+      return (
+        <div key={menuItem.id} className={classes.navMenuItem} ref={getRef}>
+          <Link to={menuItem.linkAddress}>{menuItem.label}</Link>
+          {menuItem.withSubMenu && (
+            <>
+              <IconButton
+                aria-label={menuItem.label}
+                aria-controls={menuItem.subMenuId}
+                aria-haspopup="true"
+                onClick={() => setSubMenuOpenId(menuItem.subMenuId || '')}>
+                <ArrowDropDown />
+              </IconButton>
+              <NavSubMenu
+                anchor={refsObject.current[menuItem.id]}
+                id={menuItem.subMenuId!}
+                isOpen={subMenuOpenId === menuItem.subMenuId}
+                items={menuItem.subMenuItems!}
+                onMenuClose={handleMenuClose}
+              />
+            </>
+          )}
+        </div>
+      );
+    });
 
   return (
     <AppBar position="static">
       <Toolbar>
-        <div className={classes.sectionDesktop}>
-          {renderMenuItems()}
-        </div>
+        <div className={classes.sectionDesktop}>{renderMenuItems()}</div>
         <div className={classes.sectionMobile} ref={mobileMenuRef}>
           <IconButton
             aria-label="show more"
             aria-controls={mobileMenuId}
             aria-haspopup="true"
             onClick={handleMobileMenuOpen}
-            color="inherit"
-          >
+            color="inherit">
             <MoreVert />
           </IconButton>
-          {isMobileMenuOpen
-            && (
-              <MobileNavMenu
-                anchor={mobileMenuRef.current}
-                isOpen={isMobileMenuOpen}
-                items={menuItems}
-                onMenuClose={handleMobileMenuClose}
-              />
-            )}
+          {isMobileMenuOpen && (
+            <MobileNavMenu
+              anchor={mobileMenuRef.current}
+              isOpen={isMobileMenuOpen}
+              items={menuItems}
+              onMenuClose={handleMobileMenuClose}
+            />
+          )}
         </div>
       </Toolbar>
     </AppBar>
-
-  );
-};
-
-export const MobileNavMenu: React.FC<MobileNavMenuProps> = (props: MobileNavMenuProps) => {
-  const [subMenuOpenId, setSubMenuOpenId] = useState<string>('');
-
-  const refsObject = useRef<{ [key: string]: HTMLDivElement }>({});
-
-  const handleSubMenuClose = () => {
-    setTimeout(() => setSubMenuOpenId('none'), 0);
-  };
-
-  const {
-    anchor,
-    isOpen,
-    items,
-    onMenuClose,
-  } = props;
-
-  const mobileClasses = useStyles();
-
-  const renderItems = () => items?.map((menuItem: IMenuItem) => {
-    return (
-      <MenuItem
-        key={menuItem.id}
-        onClick={onMenuClose}
-      >
-        {renderMenuItem(
-          refsObject,
-          menuItem,
-          mobileClasses.navMenuItem,
-          subMenuOpenId,
-          setSubMenuOpenId,
-          handleSubMenuClose
-        )}
-      </MenuItem>
-    );
-  });
-
-  return (
-    <Menu
-      anchorEl={anchor}
-      getContentAnchorEl={null}
-      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-      id="menu-auth"
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isOpen}
-      onClose={onMenuClose}
-    >
-      {items && renderItems()}
-    </Menu>
   );
 };
