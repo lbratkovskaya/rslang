ba63f06240155d31e6415c95fe8187b770e91a5c
Alexey Teterin <teterin.alexey@gmail.com>
2021-04-02 18:22:47 +0500

    feat: add appear animation for Savannah

--- src/components/GameSavannah/GameSavannah.tsx
+++ src/components/GameSavannah/GameSavannah.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useFullScreenHandle } from 'react-full-screen';
 import SavannahGamePlay from './SavannahGamePlay';
 import SavannahStartWindow from './SavannahStartWindow';
 import { FullScreenWrapper, FullScreenBtn } from '../commonComponents';
 import Header from '../Header';
+import { SAVANNAH } from '../../constants';
 import { IAppState } from '../../store/types';
 import useStyles from './styles';
 
@@ -14,15 +15,22 @@ const GameSavannah: React.FC = () => {
   const classes = useStyles();
   const handle = useFullScreenHandle();
   const fullScreenClass = fullSize ? ` ${classes.wrapperFull}` : ` ${classes.wrapperNotFull}`;
+  const [style, setStyle] = useState({});
 
   const handleFullSize = () => {
     setFullSize(!fullSize);
     return fullSize ? handle.exit() : handle.enter();
   };
 
+  useEffect(() => {
+    const bg = new Image();
+    bg.onload = () => setStyle({ opacity: 1 });
+    bg.src = SAVANNAH.background;
+  }, []);
+
   const gameComponent = (
     <>
-      <div className={`${classes.savannahWrapper}${fullScreenClass}`}>
+      <div className={`${classes.savannahWrapper}${fullScreenClass}`} style={style}>
         {!savannahData.isStartGame ? <SavannahStartWindow /> : <SavannahGamePlay />}
       </div>
       <FullScreenBtn changeScreen={handleFullSize} />
